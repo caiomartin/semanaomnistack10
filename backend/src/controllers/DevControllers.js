2095321fbe0b0f1471eb30310f03021f66e1e802
Caio Martins <caiofernandes14@gmail.com>
2020-01-18 13:02:38 -0300

    Creat route developer registration

--- backend/src/controllers/DevControllers.js
+++ backend/src/controllers/DevControllers.js
@@ -4,25 +4,29 @@ const Dev = require('../models/Dev')
 module.exports = {
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
-    
+        
+        let dev = await Dev.findOne({ github_username })
+
         const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
     
-        const { name = login , avatar_url, bio } = apiResponse.data
+        if (!dev) {
+            const { name = login , avatar_url, bio } = apiResponse.data
     
-        const techsArray = techs.split(',').map(techs => techs.trim())
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
+            const techsArray = techs.split(',').map(techs => techs.trim())
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            }
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                techs: techsArray,
+                location
+            })
         }
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            techs: techsArray,
-            location
-        })
          
         return response.json(dev)
     }
