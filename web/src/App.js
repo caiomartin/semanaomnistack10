2d6c3fa9e56322dab76b2019078f4e75abf82650
Caio Martins <caiofernandes14@gmail.com>
2020-01-18 19:31:22 -0300

    Frontend style created

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+
 import './global.css'
 import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 // CONCEIRO REACT
 // Componente: Bloco isolado de HTML, CSS e JS, o qual nao interfere no restante da aplicação
 // Propriedade: Informação que um componete PAI passa para o componete FILHO 
 // Estado: Infromações mantidas pelo componente (Lembrar: imutabilidade) 
 
+
+
 function App() {
+  
   return (
     <div id="app">
       <aside>
@@ -36,12 +42,63 @@ function App() {
             </div>
           </div>
 
-          <button type="submit" />
+          <button type="submit">Salvar</button>
         
          </form>
       </aside>
+
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Caio Martins"/>
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g/">Acessar perfil Github</a>
+          </li>  
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Caio Martins"/>
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g/">Acessar perfil Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Caio Martins"/>
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g/">Acessar perfil Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4" alt="Caio Martins"/>
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</p>
+            <a href="https://github.com/diego3g/">Acessar perfil Github</a>
+          </li>
+        </ul>
         
+
       </main>
     </div>
   );
